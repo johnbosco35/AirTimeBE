8129fe73ec1c5a8ed82f6ecf2b88005410f9bf59
johnbosco35 <okonkwovincent63@gmail.com>
2024-08-31 01:45:50 +0100

    added

--- index.ts
+++ index.ts
@@ -1,12 +1,21 @@
 import express, { Application } from "express";
 import { dbConnect } from "./config/db";
 import router from "./router/router";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
 
 const app: Application = express();
 
 const port = process.env.Port!;
 
+const swaggerDocs = YAML.load(
+  //   path.join(__dirname, "..", "..", "docs", "documentation.yaml")
+  path.join(__dirname, "..", "..", "docs", "documentation.yaml")
+);
+
 // Swagger UI route
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 app.use("/api/v1/user", router);
